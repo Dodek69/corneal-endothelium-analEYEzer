Added returning of metrics and fixed metrics calculation

backend/api/analysis/processing/postprocessing.py
@@ -83,13 +83,41 @@ def close_and_skeletonize(img):
     
     return skeletonize(closed_img)
 
-def calculate_metrics(skeleton_inverted, area_in_mm):
+def calculate_metrics(skeleton_inverted, area_per_pixel):
     num_labels, labelled_image, stats, _ = cv2.connectedComponentsWithStats(skeleton_inverted, 8, cv2.CV_32S, connectivity=4)
-    areas = stats[:, cv2.CC_STAT_AREA]
+    num_labels = num_labels - 1             # Ignore background label
+    areas = stats[1:, cv2.CC_STAT_AREA]     # Ignore background label
+    
+    #logger.debug(f"sum(areas): {np.sum(areas)}")
+    #logger.debug(f"max(areas): {np.max(areas)}")
     
     feature_counts, three_label_meetings = find_label_meetings(labelled_image, num_labels)
     num_hexagonal = np.count_nonzero(feature_counts == 6)
-    return num_labels/area_in_mm, np.std(areas)/np.mean(areas), num_hexagonal/num_labels, feature_counts, three_label_meetings, num_labels, labelled_image
+    #logger.debug(f"num_hexagonal: {num_hexagonal}")
+    
+    #logger.debug(f"num_labels: {num_labels}")
+    size = skeleton_inverted.size
+    #logger.debug(f"size: {size}")
+    
+    area = size * area_per_pixel
+    #logger.debug(f"area: {area}")
+    
+    cell_density = num_labels/area
+    #logger.debug(f"cell_density: {cell_density}")
+    
+    std_areas = np.std(areas)
+    #logger.debug(f"std_areas: {std_areas}")
+    
+    mean_areas = np.mean(areas)
+    #logger.debug(f"mean_areas: {mean_areas}")
+    
+    coefficient_value = std_areas / mean_areas
+    #logger.debug(f"coefficient_value: {coefficient_value}")
+    
+    hexagonal_cell_ratio = num_hexagonal / num_labels
+    #logger.debug(f"hexagonal_cell_ratio: {hexagonal_cell_ratio}")
+    
+    return num_labels, area, cell_density, std_areas, mean_areas, coefficient_value, num_hexagonal, hexagonal_cell_ratio, feature_counts, three_label_meetings, num_labels, labelled_image
 
 def find_label_meetings(labelled_image, num_labels):
     rows, cols = labelled_image.shape

backend/api/analysis/services/analysis_service.py
@@ -126,7 +126,7 @@ class AnalysisService(AbstractService):
             logger.debug('Pipeline finished')
             
             processed_data = []
-            metrics = ""
+            metrics = []
             
             for original_image, reference, prediction, file_path in zip(original_images, input_masks, predictions, input_images_paths):
                 logger.debug(f"Overlaying mask...")
@@ -146,17 +146,13 @@ class AnalysisService(AbstractService):
                 processed_data.append(AnalysisService.zip_encode(cv2.cvtColor(overlayed_image, cv2.COLOR_BGR2RGB), file_path, overlayed_path))
                 
                 logger.debug(f"Generated overlay path {str(generate_output_path(file_path, overlayed_path))}")
-                metrics = metrics + f"{file_path}\n"
                 
                 logger.debug(f"Calculating metrics for {file_path}")
                 skeleton_inverted = np.invert(prediction).astype(np.uint8)
-                cell_density, coefficient_value, hexagonal_cell_ratio, feature_counts, three_label_meetings, num_labels, labelled_image = calculate_metrics(skeleton_inverted, area_per_pixel)
+                num_labels, area, cell_density, std_areas, mean_areas, coefficient_value, num_hexagonal, hexagonal_cell_ratio, feature_counts, three_label_meetings, num_labels, labelled_image = calculate_metrics(skeleton_inverted, area_per_pixel)
 
-                logger.debug(f"Writing metrics...")
-                metrics = metrics + f"Cell Density: {cell_density}\n"
-                metrics = metrics + f"Coefficient Value: {coefficient_value}\n"
-                metrics = metrics + f"Hexagonal Cell Ratio: {hexagonal_cell_ratio}\n"
-                metrics = metrics + f"====\n"
+                logger.debug(f"Appending metrics...")
+                metrics.append((num_labels, area, cell_density, std_areas, mean_areas, coefficient_value, num_hexagonal, hexagonal_cell_ratio))
                 
                 if generate_labelled_images:
                     logger.debug(f"Generating labelled image...")
@@ -165,8 +161,6 @@ class AnalysisService(AbstractService):
                     logger.debug(f"Encoding labelled image...")
                     processed_data.append(AnalysisService.zip_encode(labels_visualization_image, file_path, labelled_images_path))
              
-            #processed_data.append((metrics.encode('utf-8'), 'metrics.txt'))
-            logger.debug(f'Metrics: {metrics}')
             
             for index, (data, filename) in enumerate(processed_data):
                 ordered_filename = f"{task_id}/{index:03d}_{filename}"
@@ -177,13 +171,13 @@ class AnalysisService(AbstractService):
             logger.debug(f'Metrics: {metrics}')
             retries = 0
             while retries < 5:
-                logger.debug(f'iteration {retries}')
+                #logger.debug(f'iteration {retries}')
                 for index, (data, filename) in enumerate(processed_data):
-                    logger.debug(f'generating ordered filename...')
+                    #logger.debug(f'generating ordered filename...')
                     ordered_filename = f"{task_id}\{index:03d}\{filename}"
-                    logger.debug(f'decoding data...')
+                    #logger.debug(f'decoding data...')
                     image_data_bytes = base64.b64decode(data.encode('utf-8'))
-                    logger.debug(f'uploading file...')
+                    #logger.debug(f'uploading file...')
                     minio_repo.upload_file_directly(image_data_bytes, ordered_filename)
                 logger.debug(f'listing files...')
                 files = minio_repo.list_files()
@@ -197,9 +191,12 @@ class AnalysisService(AbstractService):
                 retries += 1
             else:
                 logger.debug(f"upload failed after {retries} retries")
-                return processed_data, None
+                try:
+                    return (processed_data, metrics), None
+                except Exception as e:
+                    return (None, None), str(e)
             
         except Exception as e:
-            return None, str(e)
+            return (None, None), str(e)
         
-        return None, None
+        return (None, metrics), None